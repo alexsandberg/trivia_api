7511b93ead4a3da537811d3e900cd3f161934cc3
Alex Sandberg <alexsandberg7@gmail.com>
2019-10-29 16:39:51 -0600

    Add DELETE question route handler. Change db to trivia_test

--- backend/models.py
+++ backend/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = "trivia"
+database_name = "trivia_test"
 database_path = "postgres://{}/{}".format('localhost:5432', database_name)
 
 db = SQLAlchemy()
